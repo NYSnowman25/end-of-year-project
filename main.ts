327ffc0de56682bb913d8932bc26c2b568549ed3
NYSnowman25 <80698546+NYSnowman25@users.noreply.github.com>
2022-05-17 10:16:56 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
- 
+tiles.setCurrentTilemap(tilemap`level1`)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . 
+    . . . . . 5 5 5 5 . . . . 
+    . . . . 5 d d d 5 . . . . 
+    . . . . 5 d d d d . . . . 
+    . . . . 5 d 1 f d d . . . 
+    . . . . . d d d d . . . . 
+    . . . . . d d d d . . . . 
+    . . . . . . d . . . . . . 
+    . . . 2 2 7 d 7 2 2 . . . 
+    . . . 2 2 2 7 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 . . . 
+    . . . d 2 2 2 2 2 d . . . 
+    . . . . 2 2 2 2 2 . . . . 
+    . . . . 2 2 . 2 2 . . . . 
+    . . . . 2 2 . 2 2 . . . . 
+    . . . . f f . f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.ay = 500
+scene.cameraFollowSprite(mySprite)
